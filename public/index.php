8583c05fed4d468dd2ab91daaac0848ddd07c03b
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-18 22:39:48 +0300

    css good to go

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../src/templates/header.html";
+require_once "../src/templates/header.php";
 require_once "forms.php";
 echo "<h3>BODY<h3>";
 require_once "../src/templates/footer.html";

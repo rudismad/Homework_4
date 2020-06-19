a3e521628663b3607ab48736335acab383669c41
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-19 08:51:40 +0300

    login

---
+++ public/login.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    echo "Cool got POST method will save my login";
+    if (isset($_POST['myName'])) {
+        $_SESSION['myName'] = $_POST['myName'];
+        echo "Session saved";
+        header("Location: login.php");
+    } else {
+        echo "No myName set";
+    }
+
+} else {
+    echo "Not POST probably just a request " . $_SERVER['REQUEST_METHOD'];
+}

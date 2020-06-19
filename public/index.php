e784fec2910e06526e26f051228898c83c388723
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-19 13:52:09 +0300

    config to gitignore, connection with MySQL

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+// Ctrl+F5 to force cache refresh 
 require_once "../src/templates/header.php";
 require_once "forms.php";
 echo "<h3>BODY<h3>";
--- public/tasks.php
+++ public/tasks.php
@@ -1,13 +1,34 @@
 <?php
+
+session_start();
+require_once "../src/templates/header.php";
+require_once "../config/config.php";
+
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+echo "<hr>";
+
+$sql = "SELECT * FROM tasks";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+            var_dump($row);
+    // echo "id: " . $row["id"]. " - Task: " . $row["task"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();

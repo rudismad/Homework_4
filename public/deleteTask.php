45ce57b7f800e45ec2e9303caf739695e78dae20
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-20 09:17:49 +0300

    text additions

--- public/deleteTask.php
+++ public/deleteTask.php
@@ -8,7 +8,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // INSERT INTO `list` (`id`, `task`, `created`) VALUES (NULL, 'Mow the lawn', current_timestamp());
 $id = $_POST['deleteTask']; 
 $stmt = $conn->prepare("DELETE FROM `list` WHERE `list`.`id` =  (?) ");
-$stmt->bind_param("d", $id); //d means decimal here
+$stmt->bind_param("d", $id); //d means integer here
 $stmt->execute();
 
 header("Location: /tasks.php");

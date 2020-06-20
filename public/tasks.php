2b71d7855e32abe2beeba3d5a5e58ec1678a4a89
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-20 08:02:53 +0300

    addTaskForm and all set to laptop

--- public/tasks.php
+++ public/tasks.php
@@ -17,16 +17,21 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+echo "<hr>";
 
-$sql = "SELECT * FROM tasks";
+$sql = "SELECT * FROM list";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-            var_dump($row);
-    // echo "id: " . $row["id"]. " - Task: " . $row["task"]. " " . $row["lastname"]. "<br>";
-  }
+            // var_dump($row);
+        $html="id: " . $row["id"];
+        $html.="task - " . $row["task"];
+        $html.= " Created on:" . $row["created"];
+        $html.= "<hr>";
+        echo $html;
+      }
 } else {
   echo "0 results";
 }

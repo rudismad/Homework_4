deb960d2416df88b5cdc1c38bb7b637e525a4898
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-22 10:34:41 +0300

    comments

--- public/updateTask.php
+++ public/updateTask.php
@@ -5,8 +5,8 @@ if (!isset($_POST['updateTask'])) {
 require_once "../config/config.php";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$id = $_POST['updateTask']; //might want to check with if user has filled this form
-$task = $_POST['task']; //might want to check with if user has filled this form
+$id = $_POST['updateTask']; //check with if user has filled this form
+$task = $_POST['task']; //check with if user has filled this form
 // UPDATE `list` SET `task` = 'Mow the lawn until 15:00' WHERE `list`.`id` = 6$id = $_POST['updateTask']; 
 $stmt = $conn->prepare("UPDATE `list` 
 SET `task` =  (?)

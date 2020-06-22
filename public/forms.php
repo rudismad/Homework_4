caf2b429184bd75bbac5231afaa29dda4981c3cf
Rudolfs Klavins <klavins.rudolfs94@gmail.com>
2020-06-22 10:22:04 +0300

    check pw and email

--- public/forms.php
+++ public/forms.php
@@ -1,3 +1,4 @@
+
 <form action="login.php" method="post">
 <label for="login" class="add-form">Email:</label>
 <input id="login" type="email" name="myEmail" placeholder="user@todolist.com" required >
@@ -5,3 +6,8 @@
 <input id="pw" type="password" name="myPw" placeholder="password" required>
 <button type="submit">Login</button>
 </form>
+<?php
+if (isset($_POST['myPw']) && $_POST['myEmail'] != "") {
+    $myName = $_POST['myEmail'];
+    $myPw = $_POST['myPw'];
+}
